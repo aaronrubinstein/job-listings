2c5a6ed8fead1e236c052fccfef4a8fcc6169643
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-01-15 07:40:37 +0000

    Saving progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,31 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte';
 </script>
 
+<header/>
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+      <Card/>
+      <Card/>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+
+  header {
+    width: 100%;
+    height: 156px;
+    background-color: #5CA5A5;
+    background-image: url(/images/bg-header-desktop.svg);
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  main {
+    width: 100%;
+    max-width: 1110px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    margin: 76px 165px 76px 165px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+
 </style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+    Hello
+</div>
+
+<style>
+    .card {
+        height: 152px;
+        width: 100%;
+        border-radius: 5px;
+        background: #FFF;
+        padding: 32px 40px;
+        box-shadow: 0px 15px 20px -5px hsla(189, 81%, 28%, 0.15);
+    }
+</style>
